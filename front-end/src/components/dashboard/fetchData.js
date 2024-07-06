25fdc9eca31ecb7717f1b8d18a559924244d5682
AMMAR <141614147+AmmarTheDeveloper@users.noreply.github.com>
2024-07-06 20:41:53 +0530

    updated gitignore file

--- front-end/src/components/dashboard/fetchData.js
+++ front-end/src/components/dashboard/fetchData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function fetchData ( setData ) {
 
     try {
-        let response = await axios.get( "http://localhost:5000/getdata" );
+        let response = await axios.get( process.env.BASE_URL + "/getdata" );
         setData( response.data );
     } catch ( error ) {
         console.log( error );
